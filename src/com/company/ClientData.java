7064c72f844bde94729d1db25e3081f5fe6716ab
Sajeeb Sarkar <sajeeb131sarkar@gmail.com>
2021-09-26 19:10:29 +0600

    money tracker server

---
+++ src/com/company/ClientData.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class ClientData {
+    String username,fullname,balance,currency;
+    double bill,grocery,transport=0;
+
+    public ClientData(){
+
+    }
+    public ClientData(String username, String fullname, String balance,String currency) throws FileNotFoundException {
+        this.username = username;
+        this.fullname = fullname;
+        this.balance = balance;
+        this.currency=currency;
+
+        File file=new File("Files/"+username+".txt");
+        PrintWriter out=new PrintWriter(file);
+        out.println(username+"\n"+fullname+"\n"+balance+"\n"+currency+"\nend");
+        System.out.println(username+"\n"+fullname+"\n"+balance+"\n"+currency+"\nend");
+        out.close();
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,84 @@
+package com.company;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main{
+    private static HashMap<String,String> map;
+    static BufferedWriter writer;
+    static BufferedReader reader;
+    private static String username;
+    private static String password;
+
+    private static void readHashmap(HashMap<String,String> map) throws IOException{
+        Scanner sc=new Scanner(new File("Files/Hashamp/hashmap.txt"));
+        while(sc.hasNext()){
+            String key=sc.next();
+            String value=sc.next();
+            map.put(key,value);
+        }
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Server is waiting for client.");
+            ServerSocket src=new ServerSocket(2021);
+
+            map=new HashMap<>();
+            readHashmap(map);
+            FileWriter out=new FileWriter("Files/Hashamp/hashmap.txt");
+            BufferedWriter out2=new BufferedWriter(out);
+
+            while(true){
+                System.out.println(map.size());
+                Socket sc=src.accept();
+                System.out.println("server is connected to client");
+
+                InputStreamReader i=new InputStreamReader(sc.getInputStream());
+                reader=new BufferedReader(i);
+                OutputStreamWriter o=new OutputStreamWriter(sc.getOutputStream());
+                writer=new BufferedWriter(o);
+
+                String accountType=reader.readLine();
+
+                if(accountType.equals("new")){
+                    username=reader.readLine();
+                    password=reader.readLine();
+                    String fullname=reader.readLine();
+                    String balance=reader.readLine();
+                    String currency=reader.readLine();
+
+
+                    out2.append(username+"\n"+password);
+                    readHashmap(map);
+                    out2.close();
+
+                    ClientData data=new ClientData(username,fullname,balance,currency);
+                }
+                else{
+                    username=reader.readLine();
+                    System.out.println(username);
+                    password=reader.readLine();
+                    System.out.println(password);
+                    System.out.println(map.containsKey(username));
+                    if(map.containsKey(username)){
+                        writer.write("True"+"\n");
+                        writer.flush();
+                    }
+                    else{
+                        writer.write("False"+"\n");
+                        writer.flush();
+                    }
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
