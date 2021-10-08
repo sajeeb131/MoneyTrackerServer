1be83271f38ce2bd112fddb5442779060114f79d
Yeasin Hasan <yeasinhasan9822@gmail.com>
2021-10-08 20:06:41 +0600

    Client Data Class updated

--- src/com/company/ClientData.java
+++ src/com/company/ClientData.java
@@ -4,12 +4,28 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ClientData {
+public class ClientData implements Runnable{
     ArrayList<ClientData> clients=new ArrayList<>();
+
     String username,fullname,balance,currency;
-    double bill,grocery,transport,loan=0;
+    String loan="00";
+    double bill,grocery,transport,restaurant,shopping;
     BufferedReader reader;
     BufferedWriter writer;
+
+    public void readData(BufferedReader input){
+        try{
+            username=input.readLine();
+            fullname=input.readLine();
+            balance=input.readLine();
+            currency=input.readLine();
+            loan=input.readLine();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 
     public ClientData(String username,BufferedWriter writer, BufferedReader reader){
         this.writer=writer;
@@ -17,27 +33,47 @@ public class ClientData {
         try{
             File file=new File("Files/Accounts/"+username+".txt");
             BufferedReader input=new BufferedReader(new FileReader(file));
-            String user=input.readLine();
-            writer.write(user+"\n");
+            readData(input);
+            writer.write(username+"\n");
+            writer.write(fullname+"\n");
+            writer.write(balance+"\n");
+            writer.write(loan+"\n");
+            writer.write(currency+"\n");
             writer.flush();
-            System.out.println(user);
 
         }catch (Exception e){
             e.printStackTrace();
         }
-
     }
-    public ClientData(String username, String fullname, String balance,String currency) throws FileNotFoundException {
+
+    @Override
+    public void run() {
+        while (true){
+            try{
+                String category=reader.readLine();
+                String amount=reader.readLine();
+                String date=reader.readLine();
+                String Description=reader.readLine();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ClientData(String username, String fullname, String balance, String currency, BufferedWriter writer, BufferedReader reader) throws FileNotFoundException,IOException {
         this.username = username;
         this.fullname = fullname;
         this.balance = balance;
         this.currency=currency;
+        this.reader=reader;
+        this.writer=writer;
 
         File file=new File("Files/Accounts/"+username+".txt");
         PrintWriter out=new PrintWriter(file);
-        out.println(username+"\n"+fullname+"\n"+balance+"\n"+currency+"\nend");
+        out.println("start\n"+username+"\n"+fullname+"\n"+balance+"\n"+currency+"\n"+loan+"\nend");
         System.out.println(username+"\n"+fullname+"\n"+balance+"\n"+currency+"\nend");
         out.close();
-        clients.add(this);
+
     }
 }
