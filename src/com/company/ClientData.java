ae01649161f1fb5dcd1f0662a3995cb95e9abe23
Sajeeb Sarkar <sajeeb131sarkar@gmail.com>
2021-10-10 18:05:32 +0600

    User individual hashmap.txt added to initialize balance,bills,grocery,shopping etc.

--- src/com/company/ClientData.java
+++ src/com/company/ClientData.java
@@ -4,28 +4,23 @@ import jdk.jfr.Category;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class ClientData implements Runnable {
+    HashMap<String,String> map=new HashMap<>();
     ArrayList<ClientData> clients = new ArrayList();
-    String username;
-    String fullname;
-    String balance;
-    String currency;
-    String loan = "00";
-    double amount = 0.0D;
-    double bill = 0.0D;
-    double grocery = 0.0D;
-    double transport = 0.0D;
-    double restaurant = 0.0D;
-    double shopping = 0.0D;
+    String username,fullname,balance,currency;
+    String bill,grocery,transport,restaurant,shopping,loan = "00";
+    double amount;
 
     String infoHistory;
 
     BufferedReader reader;
     BufferedWriter writer;
 
+    //function to read user data from file
     public void readData(BufferedReader input) {
         try {
             this.username = input.readLine();
@@ -33,17 +28,35 @@ public class ClientData implements Runnable {
             this.balance = input.readLine();
             this.currency = input.readLine();
             this.loan = input.readLine();
-        } catch (IOException var3) {
-            var3.printStackTrace();
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
+    //to initialize categories and and balance from user hashmap file
+    private void readHashdata(){
+        balance=map.get("Balance");
+        bill=map.get("Bill");
+        transport=map.get("Transportation");
+        grocery=map.get("Grocery");
+        restaurant=map.get("Restaurant");
+        shopping=map.get("Shopping");
+        loan=map.get("Loan");
     }
 
     public ClientData(String username, BufferedWriter writer, BufferedReader reader) {
         this.writer = writer;
         this.reader = reader;
+        this.username=username;
 
         try {
+            //reading hashmap
+            readHashmap(map);
+            readHashdata();
+
+            //getting basic user data from file
+            //and writing them to client
             File file = new File("Files/Accounts/" + username + ".txt");
             BufferedReader input = new BufferedReader(new FileReader(file));
             this.readData(input);
@@ -53,10 +66,10 @@ public class ClientData implements Runnable {
             writer.write(this.loan + "\n");
             writer.write(this.currency + "\n");
             writer.flush();
-        } catch (Exception var6) {
-            var6.printStackTrace();
-        }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     private static void writeInfo(String info, File f) throws IOException{
         FileWriter fw = new FileWriter(f, true);
@@ -73,35 +86,39 @@ public class ClientData implements Runnable {
                 this.amount = Double.parseDouble(amountStr);
                 this.writer.write(category + "\n");
                 if (category.equals("Bills")) {
-                    this.bill += this.amount;
+                    bill=(Double.parseDouble(bill)+amount)+"";
+                    System.out.println(bill);
                     this.writer.write(this.bill + "\n");
                 }
 
                 if (category.equals("Grocery")) {
-                    this.grocery += this.amount;
+                    grocery=(Double.parseDouble(grocery)+amount)+"";
                     this.writer.write(this.grocery + "\n");
                 }
 
                 if (category.equals("Restaurant")) {
-                    this.restaurant += this.amount;
+                    restaurant=(Double.parseDouble(restaurant)+amount)+"";
                     this.writer.write(this.restaurant + "\n");
                 }
 
                 if (category.equals("Transport")) {
-                    this.transport += this.amount;
+                    transport=(Double.parseDouble(transport)+amount)+"";
                     this.writer.write(this.transport + "\n");
                 }
 
                 if (category.equals("Shopping")) {
-                    this.shopping += this.amount;
+                    shopping=(Double.parseDouble(shopping)+amount)+"";
                     this.writer.write(this.shopping + "\n");
                 }
+
                 this.writer.flush();
                 infoHistory=date+" "+category+": "+ amountStr+", Description: "+description+" ";
                 System.out.println(infoHistory);
                 File f = new File("Files/Accounts/" + username + ".txt");
                 writeInfo(infoHistory,f);
-            } catch (IOException e) {
+
+            }
+            catch (IOException e) {
                 e.printStackTrace();
             }
         }
@@ -113,10 +130,41 @@ public class ClientData implements Runnable {
         this.currency = currency;
         this.reader = reader;
         this.writer = writer;
+
+        //creating a new file to store basic user data taken from sign up page
         File file = new File("Files/Accounts/" + username + ".txt");
         PrintWriter out = new PrintWriter(file);
         out.println(username + "\n" + fullname + "\n" + balance + "\n" + currency + "\n" + this.loan + "\nend");
         System.out.println(username + "\n" + fullname + "\n" + balance + "\n" + currency + "\nend");
         out.close();
+
+        //Creating a file and initializing users balance, and other categories
+        FileWriter fileWriter=new FileWriter("Files/Hashamp/"+username+"MAP"+".txt",true);
+        fileWriter.write("Balance"+" "+balance+"\n"+
+                "Bills 0.0\n"+"Grocery 0.0\n"+"Transportation 0.0\n"+"Restaurant 0.0\n"+"Shopping 0.0\n"+"Loan 0.0\n");
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
+    private void updateHashmapFile() throws IOException{
+        //Creating a file and initializing users balance, and other categories
+        FileWriter fileWriter=new FileWriter("Files/Hashamp/"+username+"MAP"+".txt");
+        fileWriter.write("Balance"+" "+balance+"\n"+
+                "Bill"+" "+bill+"\n"+"Grocery"+" "+grocery+"\n"+"Transportation"+" "+transport+"\n"+
+                "Restaurant"+" "+restaurant+"\n"+"Shopping"+" "+shopping+"\n"+"Loan"+" "+loan+"\n");
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
+    //function to read individual users balance
+    //and other spending from file and put it in individual user's hashmap
+    private void readHashmap(HashMap<String,String> map) throws IOException{
+        Scanner sc=new Scanner(new File("Files/Hashamp/"+username+"MAP"+".txt"));
+        while(sc.hasNext()){
+            String key=sc.next();
+            String value=sc.next();
+            map.put(key,value);
+        }
+        sc.close();
     }
 }
